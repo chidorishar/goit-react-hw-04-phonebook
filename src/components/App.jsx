8d9e8d64a7cc9741d6afff04933c1e9122f998d5
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-13 18:45:50 +0200

    Refactor App with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { nanoid } from 'nanoid';
@@ -6,134 +6,104 @@ import { theme } from 'utils/theme';
 import { Box } from 'components/common/Box/Box.styled';
 import { ContactForm, ContactList, Filter } from './AllComponents';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const LS_KEY = 'contacts';
 
-  LS_KEY = 'contacts';
+export function App() {
+  const [contacts, setContacts] = useState(() => {
+    let readFromLSContacts = [];
 
-  componentDidMount() {
     try {
-      let readFromLSContacts = JSON.parse(localStorage.getItem(this.LS_KEY));
-      readFromLSContacts ??= [];
-      this.setState({ contacts: readFromLSContacts });
+      readFromLSContacts = JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
     } catch (error) {
       console.log(
         'There is occurred error while attempting to read data from local storage!'
       );
     }
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    let { contacts } = this.state;
-
-    contacts !== prevState.contacts &&
-      localStorage.setItem(this.LS_KEY, JSON.stringify(contacts));
-  }
-
-  onContactAdd = ({ name, number }) => {
-    if (this.hasContactWithName(name)) {
-      Notify.warning("Can't add already existing contact");
-      return;
-    }
-
-    this.setState(prevState => {
-      const normName = name.trim();
-      const updatedContacts = [
-        ...prevState.contacts,
-        { name: normName, number, id: nanoid() },
-      ];
-
-      return {
-        contacts: updatedContacts,
-      };
-    });
-  };
-
-  onRemoveContact = contactIdToRemove => {
-    this.setState(prevState => {
-      const updatedContacts = prevState.contacts.filter(
-        ({ id }) => id !== contactIdToRemove
-      );
-
-      return {
-        contacts: updatedContacts,
-      };
-    });
-  };
-
-  onContactsFiltering = filterValue => {
-    this.setState({ filter: filterValue });
-  };
-
-  getFilteredContacts = filterString => {
-    const { contacts } = this.state;
+    return readFromLSContacts;
+  });
+  const [filterString, setFilter] = useState('');
+  //filter contacts on filter value or array of contacts changed
+  const filteredContacts = useMemo(() => {
     const normalizedFilter = filterString.toLowerCase().trim();
+
+    if (!normalizedFilter) {
+      return [];
+    }
 
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalizedFilter)
     );
-  };
+  }, [contacts, filterString]);
 
-  hasContactWithName = searchName => {
-    if (!this.state.contacts) return;
+  //on contacts changed
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  function onContactAdd({ name, number }) {
+    if (hasContactWithName(name)) {
+      Notify.warning("Can't add already existing contact");
+      return;
+    }
+
+    setContacts(prevState => {
+      const normName = name.trim();
+      const updatedContacts = [
+        ...prevState,
+        { name: normName, number, id: nanoid() },
+      ];
+
+      return updatedContacts;
+    });
+  }
+
+  function onRemoveContact(contactIdToRemove) {
+    setContacts(prevState =>
+      prevState.filter(({ id }) => id !== contactIdToRemove)
+    );
+  }
+
+  function hasContactWithName(searchName) {
+    if (!contacts) return;
 
     const searchNameNormalized = searchName.trim().toLowerCase();
 
-    return this.state.contacts.some(
+    return contacts.some(
       ({ name }) => name.toLowerCase() === searchNameNormalized
     );
-  };
+  }
 
-  render() {
-    const {
-      onContactAdd,
-      onContactsFiltering,
-      onRemoveContact,
-      getFilteredContacts,
-      state: { contacts, filter },
-    } = this;
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        width="wide"
+        m={[3]}
+        p={[3]}
+        textAlign="center"
+        borderRadius="small"
+        boxShadow="medium"
+      >
+        <Box margin="0 auto" color="textColored">
+          <h1>Phonebook</h1>
+          <ContactForm onSubmitCallback={onContactAdd} />
 
-    const filteredContacts = filter ? getFilteredContacts(filter) : contacts;
-
-    return (
-      <ThemeProvider theme={theme}>
-        <Box
-          width="wide"
-          m={[3]}
-          p={[3]}
-          textAlign="center"
-          borderRadius="small"
-          boxShadow="medium"
-        >
-          <Box margin="0 auto" color="textColored">
-            <h1>Phonebook</h1>
-            <ContactForm onSubmitCallback={onContactAdd} />
-
-            <Box
-              width={0.85}
-              margin="0 auto"
-              mt={[4]}
-              borderColor="accentSecondary"
-              color="textColoredSecondary"
-            >
-              <h2>Contacts</h2>
-              <Filter value={filter} onInputCallback={onContactsFiltering} />
-              <ContactList
-                contacts={filteredContacts}
-                onContactRemoveCallback={onRemoveContact}
-              />
-            </Box>
+          <Box
+            width={0.85}
+            margin="0 auto"
+            mt={[4]}
+            borderColor="accentSecondary"
+            color="textColoredSecondary"
+          >
+            <h2>Contacts</h2>
+            <Filter value={filterString} onInputCallback={setFilter} />
+            <ContactList
+              contacts={filterString ? filteredContacts : contacts}
+              onContactRemoveCallback={onRemoveContact}
+            />
           </Box>
         </Box>
-      </ThemeProvider>
-    );
-  }
+      </Box>
+    </ThemeProvider>
+  );
 }
